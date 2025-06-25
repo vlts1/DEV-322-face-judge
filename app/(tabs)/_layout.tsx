fda9bccc3892764c7b566e1be541db6d6067028f
jonesnbellevuecollege <n.jones@bellevuecollege.edu>
2025-06-24 20:11:18 -0700

    Added home landing page + tweaks to ui, centering, colors, etc.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
+import { Tabs } from 'expo-router';
 import { Platform } from 'react-native';
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
@@ -7,7 +7,7 @@ import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { MaterialIcons } from '@expo/vector-icons';
-// by vlad & zoe
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
@@ -24,17 +24,18 @@ export default function TabLayout() {
           default: {},
         }),
       }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'History',
-          headerTitleStyle: {fontSize: 19},
-          tabBarIcon: ({ color }) => 
-            Platform.OS === "ios" 
-              ? <IconSymbol size={28} name="newspaper.fill" color={color} />
-              : <MaterialIcons name="newspaper" size={24} color={color}/>,
-        }}
-      />
+              <Tabs.Screen
+                name="home"
+                options={{
+                  title: 'Home',
+                  headerTitleStyle: {fontSize: 19},
+                  tabBarIcon: ({ color }) =>
+                    Platform.OS === "ios"
+                      ? <IconSymbol size={28} name="house.fill" color={color} />
+                      : <MaterialIcons name="home" size={24} color={color}/>,
+                }}
+              />
+
       <Tabs.Screen
         name="scan"
         options={{
@@ -46,6 +47,18 @@ export default function TabLayout() {
               : <MaterialIcons name="camera" size={24} color={color}/>,
         }}
       />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'History',
+          headerTitleStyle: {fontSize: 19},
+          tabBarIcon: ({ color }) =>
+            Platform.OS === "ios"
+              ? <IconSymbol size={28} name="newspaper.fill" color={color} />
+              : <MaterialIcons name="newspaper" size={24} color={color}/>,
+        }}
+      />
+
     </Tabs>
   );
 }
