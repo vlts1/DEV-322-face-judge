ea8782a3ce3c8835dac4c22cc44238361d18f9cd
jonesnbellevuecollege <n.jones@bellevuecollege.edu>
2025-06-24 23:28:37 -0700

    Update dhistory page and dbwrapper

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,10 +4,13 @@ import { Colors } from '@/constants/Colors';
 import { useDBWrapper } from '@/hooks/useDBWrapper';
 import { useFocusEffect } from 'expo-router';
 import { useCallback } from 'react';
-import { ScrollView, StyleSheet, View, useColorScheme } from 'react-native';
+// adding imports to clear history button -nick
+import * as FileSystem from 'expo-file-system';
+import { ScrollView, StyleSheet, View, useColorScheme, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // By vlad & zoe
 export default function FaceJudgeMain() {
-  const { history, refreshHistory } = useDBWrapper();
+  const { history, refreshHistory, clearHistory } = useDBWrapper();
   const colorScheme = useColorScheme();
 
   const dividerColor =
@@ -21,29 +24,33 @@ export default function FaceJudgeMain() {
     );
 
   if (history.length === 0) return <EmptyHistory />;
-    
+
+ // added clear history feature to remove excess images in local hist.
   return (
-    <ScrollView
-      contentInsetAdjustmentBehavior="automatic"
-      style={styles.scrollView}
-    >
-      {history.map((item, index) => (
-        <View key={item.timestamp}>
-          <InListItem
-            id={item.timestamp}
-            emoji={item.emoji}
-            suggestion={item.suggestion}
-            imageUrl={item.imageUrl}
-          />
+    <>
+      <Button title="Clear History" onPress={clearHistory} color="#800080" />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={styles.scrollView}
+      >
+        {history.map((item, index) => (
+          <View key={item.timestamp}>
+            <InListItem
+              id={item.timestamp}
+              emoji={item.emoji}
+              suggestion={item.suggestion}
+              imageUrl={item.imageUrl}
+            />
 
-          {index < history.length - 1 && (
-            <View style={[styles.divider, { backgroundColor: dividerColor }]} />
-          )}
-        </View>
-      ))}
+            {index < history.length - 1 && (
+              <View style={[styles.divider, { backgroundColor: dividerColor }]} />
+            )}
+          </View>
+        ))}
 
-      <View style={{paddingBottom: 60 }}/>
-    </ScrollView>
+        <View style={{paddingBottom: 60 }} />
+      </ScrollView>
+    </>
   );
 }
 
